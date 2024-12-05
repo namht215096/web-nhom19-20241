84f056fc596e5d75fd16d129cb6f0b2c58549a3e
VanMinh153 <minhnguyenvh.704@gmail.com>
2024-12-05 22:54:34 +0700

    Fix Homepage: show PC, Linh kien ban chay

--- my-react-app/src/components/Home.js
+++ my-react-app/src/components/Home.js
@@ -2,16 +2,14 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { Link } from "react-router-dom";
-import { formatCash } from "../utils/formatCash";
 import { ProductsCard } from "./ProductsCard";
 
 function Home() {
   const [products, setProducts] = useState([]);
   const [productsPC, setProductsPC] = useState([]);
   const [productsVGA, setProductsVGA] = useState([]);
-  const [numProducts, setNumProducts] = useState(5); 
-  const [sortedproducts, setsortedProducts] = useState([]);
-  
+  const [numProducts, setNumProducts] = useState(5);
+
   useEffect(() => {
     const updateNumProducts = () => {
       const width = window.innerWidth;
@@ -27,10 +25,10 @@ function Home() {
 
     };
 
-    updateNumProducts(); 
+    updateNumProducts();
     window.addEventListener("resize", updateNumProducts);
 
-    return () => window.removeEventListener("resize", updateNumProducts); 
+    return () => window.removeEventListener("resize", updateNumProducts);
   }, []);
 
   useEffect(() => {
@@ -84,112 +82,112 @@ function Home() {
   return (
     <div>
       <div className="mb-4 mt-20">
-          <Navbar />
+        <Navbar />
       </div>
       <div className="lg:mx-20 flex-row">
         <div className="flex ">
           <div className="w-1/5 bg-gray-100 p-4 mr-4 hidden lg:block">
-          <ul className="xl:space-y-11 lg:space-y-6 py-2 w-40">
-            <li className="relative flex items-center justify-between">
-              <Link
-                to="/productlist"
-                className="flex items-center hover:font-bold hover:text-red-600"
-              >
+            <ul className="xl:space-y-11 lg:space-y-6 py-2 w-40">
+              <li className="relative flex items-center justify-between">
+                <Link
+                  to="/productlist"
+                  className="flex items-center hover:font-bold hover:text-red-600"
+                >
 
-                <img src="/all.svg" alt="all" />
-                <div className="laptop mr-2"></div> Tất cả sản phẩm
-              </Link>
-            </li>
-            <li className="flex items-center justify-between ">
-              <Link
-                to="/productlist/laptop"
-                className="flex items-center hover:font-bold hover:text-red-600"
-              >
+                  <img src="/all.svg" alt="all" />
+                  <div className="laptop mr-2"></div> Tất cả sản phẩm
+                </Link>
+              </li>
+              <li className="flex items-center justify-between ">
+                <Link
+                  to="/productlist/laptop"
+                  className="flex items-center hover:font-bold hover:text-red-600"
+                >
 
-                <img src="/lap.svg" alt="laptop" />
-                <div className="gamepad mr-2"></div> Laptop
-              </Link>
-            </li>
-            <li className="flex lg:flex items-center justify-between hover:font-bold hover:text-red-600">
-              <Link
-                to="/productlist/pc"
-                className="flex items-center hover:font-bold hover:text-red-600"
-              >
-                <img src="/pc.svg" alt="pc" />
-                <div className="desktop mr-2"></div> PC
-              </Link>
-            </li>
-            <li className="flex items-center justify-between">
-              <Link
-                to="/productlist/vga" 
-                className="flex items-center hover:font-bold hover:text-red-600"
-              >
-                <img src="/comp.svg" alt="Vga" />
-                <div className="gamepad mr-2"></div> VGA
-              </Link>
-            </li>
-            <li className="flex items-center justify-between">
-              <Link
-                to="/productlist/cpu" 
-                className="flex items-center hover:font-bold hover:text-red-600"
-              >
-                <img src="/comp.svg" alt="cpu" />
-                <div className="gamepad mr-2"></div> CPU
-              </Link>
-            </li>
-            <li className="flex items-center justify-between">
-              <Link
-                to="/productlist/case"
-                className="flex items-center hover:font-bold hover:text-red-600"
-              >
-                <img src="/case.svg" alt="case" />
-                <div className="gamepad mr-2"></div> Case
-              </Link>
-            </li>
-            <li className="flex items-center justify-between">
-              <Link
-                to="/productlist/screen"
-                className="flex items-center hover:font-bold hover:text-red-600"
-              >
-                <img src="/screen.svg" alt="monitor" />
-                <div className="gamepad mr-2"></div> Màn hình
-              </Link>
-            </li>
-            <li className="flex items-center justify-between">
-              <Link
-                to="/productlist/keyboard"
-                className="flex items-center hover:font-bold hover:text-red-600"
-              >
-                <img src="/acs.svg" alt="acs" />
-                <div className="gamepad mr-2"></div> Bàn phím
-              </Link>
-            </li>
-            <li className="flex items-center justify-between">
-              <Link
-                to="/productlist/mouse"
-                className="flex items-center hover:font-bold hover:text-red-600"
-              >
-                <img src="/mouse.svg" alt="mouse" />
-                <div className="gamepad mr-2"></div> Chuột
-              </Link>
-            </li>
-            <li className="flex items-center justify-between">
-              <Link
-                to="/productlist/headphone"
-                className="flex items-center hover:font-bold hover:text-red-600"
-              >
-                <img src="/sup.svg" alt="headphone" />
-                <div className="gamepad mr-2"></div> Tai nghe
-              </Link>
+                  <img src="/lap.svg" alt="laptop" />
+                  <div className="gamepad mr-2"></div> Laptop
+                </Link>
+              </li>
+              <li className="flex lg:flex items-center justify-between hover:font-bold hover:text-red-600">
+                <Link
+                  to="/productlist/pc"
+                  className="flex items-center hover:font-bold hover:text-red-600"
+                >
+                  <img src="/pc.svg" alt="pc" />
+                  <div className="desktop mr-2"></div> PC
+                </Link>
+              </li>
+              <li className="flex items-center justify-between">
+                <Link
+                  to="/productlist/vga"
+                  className="flex items-center hover:font-bold hover:text-red-600"
+                >
+                  <img src="/comp.svg" alt="Vga" />
+                  <div className="gamepad mr-2"></div> VGA
+                </Link>
+              </li>
+              <li className="flex items-center justify-between">
+                <Link
+                  to="/productlist/cpu"
+                  className="flex items-center hover:font-bold hover:text-red-600"
+                >
+                  <img src="/comp.svg" alt="cpu" />
+                  <div className="gamepad mr-2"></div> CPU
+                </Link>
+              </li>
+              <li className="flex items-center justify-between">
+                <Link
+                  to="/productlist/case"
+                  className="flex items-center hover:font-bold hover:text-red-600"
+                >
+                  <img src="/case.svg" alt="case" />
+                  <div className="gamepad mr-2"></div> Case
+                </Link>
+              </li>
+              <li className="flex items-center justify-between">
+                <Link
+                  to="/productlist/screen"
+                  className="flex items-center hover:font-bold hover:text-red-600"
+                >
+                  <img src="/screen.svg" alt="monitor" />
+                  <div className="gamepad mr-2"></div> Màn hình
+                </Link>
+              </li>
+              <li className="flex items-center justify-between">
+                <Link
+                  to="/productlist/keyboard"
+                  className="flex items-center hover:font-bold hover:text-red-600"
+                >
+                  <img src="/acs.svg" alt="acs" />
+                  <div className="gamepad mr-2"></div> Bàn phím
+                </Link>
+              </li>
+              <li className="flex items-center justify-between">
+                <Link
+                  to="/productlist/mouse"
+                  className="flex items-center hover:font-bold hover:text-red-600"
+                >
+                  <img src="/mouse.svg" alt="mouse" />
+                  <div className="gamepad mr-2"></div> Chuột
+                </Link>
+              </li>
+              <li className="flex items-center justify-between">
+                <Link
+                  to="/productlist/headphone"
+                  className="flex items-center hover:font-bold hover:text-red-600"
+                >
+                  <img src="/sup.svg" alt="headphone" />
+                  <div className="gamepad mr-2"></div> Tai nghe
+                </Link>
 
-            </li>
-          </ul>
+              </li>
+            </ul>
           </div>
           <div className="lg:w-4/5 w-full ">
-            <div className="flex ">    
-              <div className="w-full cursor-pointer">  
+            <div className="flex ">
+              <div className="w-full cursor-pointer">
                 <img src="nvidiabaner.jpg" alt="ad" />
-              </div>  
+              </div>
             </div>
             <div className="grid grid-cols-3 gap-4 mt-4">
               <div className="items-center">
@@ -236,8 +234,8 @@ function Home() {
               <div className="flex justify-between items-center mb-4">
                 <div className="flex space-x-4"></div>
               </div>
-              <div className={`grid grid-cols-5 lg:gap-3 gap-1`}>
-                {products.slice(0, 5).map((product) => (
+              <div className={`grid grid-cols-${numProducts} lg:gap-3 gap-1`}>
+                {productsPC.slice(rndInt, rndInt + numProducts).map((product) => (
                   <Link
                     to={`/productdetail/${product.product_id}`}
                     key={product.product_id}
@@ -246,7 +244,7 @@ function Home() {
                       key={product.product_id}
                       className="product-card p-4 rounded-lg shadow hover:shadow-lg"
                     >
-                     <ProductsCard product={product} />
+                      <ProductsCard product={product} />
                     </div>
                   </Link>
                 ))}
@@ -262,7 +260,7 @@ function Home() {
                 <div className="flex space-x-4"></div>
               </div>
               <div className={`grid grid-cols-5 lg:gap-3 gap-1`}>
-                {products.slice(0, 5).map((product) => (
+                {productsVGA.slice(0, 5).map((product) => (
                   <Link
                     to={`/productdetail/${product.product_id}`}
                     key={product.product_id}
